85ce0ffdd122e65b2279ad5337011e5bc4919a7e
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-03 19:21:08 +0300

    shadow password

--- Command-Line-Test/command_line_test.sh
+++ Command-Line-Test/command_line_test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-timeout 4s
-exit 1
-
 color="\033[36;40m"
 none="\033[0m"
 
@@ -22,8 +19,18 @@ case $input in
 	1) echo -e $color"Sign Up Screen"$none
 		echo
 		read -p "Please chose your username: " username
-		read -p "Please enter your password: " password
-		read -p "Please re enter your password: " password1
+		echo "$username" >> signup.txt
+		
+		if grep "$username" signup.txt
+		then
+			echo "Username $username exists: Please choose some other name."
+			read -p "Please chose your username: " username
+		fi
+
+		read -s -p "Please enter your password: " password
+		echo
+		read -s -p "Please re enter your password: " password1
+		echo
 		if [ $password == $password1 ]
 		then
 			echo "Registration Successful.Please hit any key to continue"
