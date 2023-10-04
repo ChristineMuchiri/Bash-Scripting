125637ffa12164e683ed443383b42a212800768d
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-04 22:50:21 +0300

    sum pattern

---
+++ Assignments/sum_pattern.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+read number
+
+for i in 1  
+do
+	echo -n $((number + 1))
+	echo $((number + 2))
+	echo -n $((number +3))
+	echo -n $((number +4))
+	echo $((number +5))
+	echo -n $((number +6))
+	echo -n $((number +7))
+	echo -n $((number +8))
+	echo $((number +9))
+	
+done
+
