b46cb7efc431dc3979660d8fcbecd004cbe6fc2f
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-05 22:10:53 +0300

    adding numbers

--- Assignments/add.sh
+++ Assignments/add.sh
@@ -2,5 +2,11 @@
 
 echo "Enter the numbers to addition" 
 read number1 number2
-ans=$( echo "expr $number1 + $number2" | bc -l )
-echo "Answer is $ans"
+if [[ $number1 =~ ^[0-9]+$ && $number2 =~ ^[0-9]+$ ]]
+then 
+	ans=$(expr $number1 + $number2 )
+	echo "Answer is $ans"
+else
+	ans=$(echo "$number1 + $number2" | bc -l)
+	echo "Answer is $ans"
+fi
