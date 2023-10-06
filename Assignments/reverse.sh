4a80836222cef0108a7af2f508af3662a2abd7b8
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-06 21:12:59 +0300

    reverse

--- Assignments/reverse.sh
+++ Assignments/reverse.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
 args=("$@")
+length=${#args[@]}
 
 if [ $# -eq 0 ]
 then
 	echo "Error: No argument passed"
 else
-	for (( i = ${#args[@]} - 1; i >= 0; i--))
-	do
-		echo "${args[i]}"
-	done
+	
+		if [ "${args[-1]}" == 0 ]
+		then
+			new_args=("${args[@]:0:$((length - 1))}")
+			for (( i = ${#new_args[@]} - 1; i >=0; i--))
+			do
+				echo "${new_args[i]}"
+			done
+		else
+			for (( i = ${#args[@]} -1; i >= 0; i--))
+			do
+				echo "${args[i]}"
+			done
+		fi
+	
 fi
 
