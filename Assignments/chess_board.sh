6d432ee4caf02a03251749e9aa8bcf556e83597c
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-08 21:52:46 +0300

    chess board

--- Assignments/chess_board.sh
+++ Assignments/chess_board.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-echo -e -n "\e[41m  \e[0m"
-echo -e -n "\e[47m  \e[0m"
+red_box="\e[41m   \e[0m"
+white_box="\e[47m   \e[0m"
+
+read -p "Enter the width: " width
+wid=$(($width / 2))
+for ((i=0; i<= "$wid"; i++))
+do
+	echo -e $red_box
+	echo -e $white_box
+
+
+done
+
+
 
 
