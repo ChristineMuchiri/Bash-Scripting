7fe63211619d03892a067625c0b682571e11da6e
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-08 19:32:18 +0300

    pending

--- Assignments/digit_arithmetic.sh
+++ Assignments/digit_arithmetic.sh
@@ -17,5 +17,17 @@ case $operator in
 		done
 		echo "Sum is $total_sum"
 		;;
+	-)
+		sub=0
+		for number in "${args[@]}"
+		do
+			while [ "$number" -gt 0 ]
+			do
+				digit=$((number % 10))
+				total_sub=$((digit - total_sub))
+				number=$((number / 10))
+			done
+		done
+		echo "Sub is $total_sub"
 esac
 		
