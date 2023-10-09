bb9977a4b9ec6eccd3ef1334948a2a24e0f6f1c5
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-09 23:15:25 +0300

    ascending

--- Assignments/ascend_descend.sh
+++ Assignments/ascend_descend.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
 
-arg="#@"
-array=${arg[@]}
+arg="$@"
+array="${arg[@]}"
 
 echo "1. Ascending"
 echo "2. Descending"
+echo $array
+read -p "Please select an option: " option
+
+function bubble_sort {
+	local arr=("$@")
+	local length="${#arr[@]}"
+	local swapped
+
+	while true
+	do
+		swapped=false
+		for ((i=1; i<length; i++))
+		do
+			if ((arr[i-1] > arr[i]));
+			then
+				temp="${arr[i-1]}"
+				arr[i-1]="{arr[i]}"
+				arr[i]="$temp"
+				swapped=true
+			fi
+		done
+		if ! "$swapped"
+		then
+			break
+		fi
+	done
+	echo "${arr[@]}"
+}
+
+case $option in
+	1)
+		sorted_numbers=($(bubble_sort "${arr[@]}"))
+		echo "Sorted array is ${sorted_numbers[@]}"
+		
+esac
