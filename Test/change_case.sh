c08650ea9d1e920e332871a497d0f0fb108304af
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-11 18:54:37 +0300

    change case

--- Test/change_case.sh
+++ Test/change_case.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-read -p "Enter any string in Lower Case: " lowercase
-echo -n "Result: " 
-echo "$lowercase" | tr '[:lower:]' '[:upper:]'
-read -p "Enter any string in Upper Case: " uppercase
-echo -n "Result: "
-echo "$uppercase" | tr '[:upper:]' '[:lower:]'
+#read -p "Enter any string in Lower Case: " lowercase
+#echo -n "Result: " 
+#echo "$lowercase" | tr '[:lower:]' '[:upper:]'
+#read -p "Enter any string in Upper Case: " uppercase
+#echo -n "Result: "
+#echo "$uppercase" | tr '[:upper:]' '[:lower:]'
+
+
+if [ -f $1 ]
+then
+	echo "$1" | tr '[:lower:]' '[:upper:]'
+fi
+
+if [ -d $2 ]
+then
+	echo "$2" | tr '[:lower:]' '[:upper:]'
+fi
+
