af75cf33eaae506a5fe85e3fb23f1e7d1e8315b2
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-11 22:16:02 +0300

    head and tail

--- Assignments/file_filter.sh
+++ Assignments/file_filter.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-start_line="$1"
-num_lines="$2"
-file_name="$3"
+head -5 myfile.txt | tail -n +3 | head -n 3
+string_to_compare="myfile.txt"
+arg="$1"
+if [ "$arg" = "$string_to_compare" ]
+then
+	echo "Usage : ./file_filter.sh start_line uptoline filename"
+	echo "Example: ./20_file_filter.sh 5 5 <file>"
+fi
 
-
-head -n $1 $3 | tail -n +$1 | head -n $2
  
