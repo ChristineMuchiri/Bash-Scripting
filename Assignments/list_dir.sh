fd69dd79a07d1b976b354860e7adf2dc5af4b254
ChristineMuchiri <tinniemuchiri@gmail.com>
2023-10-12 19:34:42 +0300

    ls

---
+++ Assignments/list_dir.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+directories="$@"
+
+if [ $? == 0 ]
+then
+	for item in *
+	do
+		echo "$item"
+	done
+else
+	for dir in "${directories[@]}"
+	do
+		echo $dir
+		find "$dir"
+		echo
+	done
+fi
